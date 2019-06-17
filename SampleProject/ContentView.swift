8e1be131f0726c29fa435b5b2c1f190dc7c7fc95
Sakai Kuniya <k_sakai@waku-2.com>
2019-06-17 11:22:40 +0900

    Initial Commit

---
+++ SampleProject/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SampleProject
+//
+//  Created by 酒井 邦也 on 2019/06/17.
+//  Copyright © 2019 酒井 邦也. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
